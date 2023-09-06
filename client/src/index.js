9b22b21eab098634663abbbdd9e348a329e37ed1
dishon18 <dishon.mmanyi@aol.com>
2023-09-06 13:48:58 -0400

    finished editing

--- client/src/index.js
+++ client/src/index.js
@@ -5,91 +5,91 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
 }
 // scroll sections
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
-            // active navbar links
-            navlinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-            // active sections for animation on scroll 
-            sec.classList.add("show-animate");
-        }
-        // if want to use animation that repeats use this
-        else {
-            sec.classList.remove('show-animate')
-        }
-    });
-    // sticky header
-    let header = document.querySelector('header');
+    if (top >= offset && top < offset + height) {
+      // active navbar links
+      navlinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+      // active sections for animation on scroll 
+      sec.classList.add("show-animate");
+    }
+    // if want to use animation that repeats use this
+    else {
+      sec.classList.remove('show-animate')
+    }
+  });
+  // sticky header
+  let header = document.querySelector('header');
 
-    header.classList.toggle('sticky', window.scrollY > 100);
+  header.classList.toggle('sticky', window.scrollY > 100);
 
-    // remove toggle icon and navbar when user clicks on navbar link
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
+  // remove toggle icon and navbar when user clicks on navbar link
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
 
-    // animation footer on scroll 
-    let footer = document.querySelector('footer');
-    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+  // animation footer on scroll 
+  let footer = document.querySelector('footer');
+  footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 
-    
-      
+
+
 }
 
 const contactForm = document.getElementById('contact-form');
 const submitButton = document.getElementById('submit-button');
 const loaderIcon = document.getElementById('loading-container');
 
-    //form submission handler
-    document.getElementById('contact-form').addEventListener('submit', (e) => {
-        e.preventDefault();
-        loaderIcon.style.display = 'block';
-        // Disable the submit button to prevent multiple submissions
-        submitButton.disabled = true;
+//form submission handler
+document.getElementById('contact-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  loaderIcon.style.display = 'block';
+  // Disable the submit button to prevent multiple submissions
+  submitButton.disabled = true;
 
-        const formData = new FormData(e.target);
+  const formData = new FormData(e.target);
 
-        fetch('/send-email', {
-          method: 'POST',
-          body: new URLSearchParams(formData),
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded', 
-          },
-        })
-          .then((response) => response.text())
-          .then((data) => {
-            loaderIcon.style.display = 'none';
-            const successModal = document.getElementById('successModal');
-            successModal.style.display = 'block';
-            contactForm.reset();
-          })
-          .catch((error) => {
-            console.error('Error:', error);
-          })
-          .finally(() => {
-            // Re-enable the submit button after the request is complete
-            submitButton.disabled = false;
-          });
-          
-      });
-
-    const closeSuccessModalBtn = document.getElementById('closeSuccessModal');
-
-    closeSuccessModalBtn.addEventListener('click', () => {
-        const successModal = document.getElementById('successModal');
-        successModal.style.display = 'none';
+  fetch('/send-email', {
+    method: 'POST',
+    body: new URLSearchParams(formData),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      loaderIcon.style.display = 'none';
+      const successModal = document.getElementById('successModal');
+      successModal.style.display = 'block';
+      contactForm.reset();
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    })
+    .finally(() => {
+      // Re-enable the submit button after the request is complete
+      submitButton.disabled = false;
     });
+
+});
+
+const closeSuccessModalBtn = document.getElementById('closeSuccessModal');
+
+closeSuccessModalBtn.addEventListener('click', () => {
+  const successModal = document.getElementById('successModal');
+  successModal.style.display = 'none';
+});
 
--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path'); // Import the 'path' module
 const app = express(); //making express application
-const port = process.env.PORT ||3000; // port number server will run in
+const port = process.env.PORT || 3000; // port number server will run in
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
@@ -15,49 +15,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //http endpoint to render the index.html file
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/src/index.html'));
-  });
+  res.sendFile(path.join(__dirname, '../client/src/index.html'));
+});
 
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST, 
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
   auth: {
-    user: process.env.SMTP_USER, 
-    pass: process.env.SMTP_PASS, 
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
   secure: false, // Set this to true if you want to use a secure connection (STARTTLS)
-  });
+});
 
-  //http endpoint to handle email sending functionality
+//http endpoint to handle email sending functionality
 app.post('/send-email', (req, res) => {
-const { name, email, number, subject, message } = req.body;
+  const { name, email, number, subject, message } = req.body;
 
 
-const mailOptions = {
+  const mailOptions = {
     from: process.env.SMTP_USER,
     to: process.env.SMTP_USER,
     subject: `Email from Portfolio Website: ${subject}`,
     text: `Name: ${name}\nEmail: ${email}\nNumber: ${number}\nMessage: ${message}`,
     replyTo: email,
-};
+  };
 
-transporter.sendMail(mailOptions, (error, info) => {
+  transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-        console.error('Error sending email:', error);
-        // Log the error message
-        res.status(500).send(`Error sending email: ${error.message}`);
+      console.error('Error sending email:', error);
+      // Log the error message
+      res.status(500).send(`Error sending email: ${error.message}`);
     } else {
-        console.log('Email sent:', info.response);
-        res.status(200).send('Email sent successfully');
+      console.log('Email sent:', info.response);
+      res.status(200).send('Email sent successfully');
     }
-});
-});
-  
-
-
-  // starts an http server to listen for requests
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
   });
-  
+});
+
+
+
+// starts an http server to listen for requests
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 
