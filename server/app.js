377ffa89efafab0bf4578782d04cdc6ef5bbfe95
dishon18 <dishon.mmanyi@aol.com>
2023-09-06 12:11:14 -0400

    mfinished front end of website

---
+++ server/app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const path = require('path'); // Import the 'path' module
+const app = express(); //making express application
+const port = process.env.PORT ||3000; // port number server will run in
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+// Serve static files from the 'client folder
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Use bodyParser middleware to parse JSON and form data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//http endpoint to render the index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/src/index.html'));
+  });
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST, 
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER, 
+    pass: process.env.SMTP_PASS, 
+  },
+  secure: false, // Set this to true if you want to use a secure connection (STARTTLS)
+  });
+
+  //http endpoint to handle email sending functionality
+app.post('/send-email', (req, res) => {
+    console.log('Received request with body:', req.body);
+const { name, email, number, subject, message } = req.body;
+console.log("Entering /send-email endpoint");
+console.log(`user from env variable ${process.env.SMTP_USER}`);
+console.log(`Name: ${name} Email: ${email} Number: ${number} `);
+
+const mailOptions = {
+    from: process.env.SMTP_USER,
+    to: process.env.SMTP_USER,
+    subject: `Email from Portfolio Website: ${subject}`,
+    text: `Name: ${name}\nEmail: ${email}\nNumber: ${number}\nMessage: ${message}`,
+    replyTo: email,
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.error('Error sending email:', error);
+        // Log the error message
+        res.status(500).send(`Error sending email: ${error.message}`);
+    } else {
+        console.log('Email sent:', info.response);
+        res.status(200).send('Email sent successfully');
+    }
+});
+});
+  
+
+
+  // starts an http server to listen for requests
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
+
